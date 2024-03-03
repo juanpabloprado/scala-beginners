b97b2215d2b5d373cf75c3d3cb3e1c73363303ae
Juan Prado <juan@juanpabloprado.com>
2024-03-02 18:48:03 -0600

    add functions and recursion

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,24 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  val x: Int = 42
+  println(x)
+
+  val aString: String = "hello"
+  val anotherString = "goodbye"
+
+  val aBoolean: Boolean = true
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 5273985273895237L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // variables
+  var aVariable: Int = 4
+  aVariable = 5 // side effects
+  
+
+}
